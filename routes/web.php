5c38fceee1e36bfe9b6752d6e85945d332b3b793
29a6666a92 <29a6666a92@gmail.com>
2021-01-07 11:41:06 +0700

    reset

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\Profile;
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,95 +23,3 @@ Route::get('/', function () {
 });
 
 
-/**
- * 
- *  One To Many
- */
-
- Route::get('/create_user',function(){
-     $user = User::create([
-         'name' => 'Fikri',
-         'email' => 'fikri@gmail.com',
-         'password' => bcrypt('password')
-     ]);
-     return $user;
-
- });
-
- Route::get('/create_user_posts',function(){
-     $user = User::findOrFail(2);
-
-     $user->posts()->create([
-         'title' => 'Second Post by Hafid',
-         'body' => 'HEHEHHEHEHAHAHHA'
-     ]);
-
-     return $user;
- });
-
- Route::get('/read_user_posts',function(){
-     $user = User::findOrFail(2);
-
-     $posts = $user->posts()->get();
-
-     foreach($posts as $post){
-         $data[] = [
-             'user_id' => $post->user_id,
-             'name' => $post->user->name,
-             'email' => $post->user->email,
-             'id' => $post->id,
-             'title' => $post->title,
-             'body' => $post->body,
-         ];
-     }
-
-     return $data;
-
- });
-
- Route::get('update_user_posts',function(){
-     $user = User::findOrFail(1);
-
-     $user->posts()->where('id',1)->update([
-         'title' => 'Fall in love in january',
-         'body' => 'by the way it\'s aprial moops'
-     ]);
-
-      $posts = $user->posts()->get();
-
-     foreach($posts as $post){
-         $data[] = [
-             'user_id' => $post->user_id,
-             'name' => $post->user->name,
-             'email' => $post->user->email,
-             'id' => $post->id,
-             'title' => $post->title,
-             'body' => $post->body,
-         ];
-     }
-
-     return $data;
-
- });
-
- Route::get('delete_user_posts',function(){
-     $user = User::findOrFail(2);
-
-     $user->posts()->where('id',3)->delete();
-
-      $posts = $user->posts()->get();
-
-     foreach($posts as $post){
-         $data[] = [
-             'user_id' => $post->user_id,
-             'name' => $post->user->name,
-             'email' => $post->user->email,
-             'id' => $post->id,
-             'title' => $post->title,
-             'body' => $post->body,
-         ];
-     }
-
-     return $data;
-
- });
